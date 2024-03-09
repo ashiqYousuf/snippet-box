84434d0207ed20f778469d6c9998f17875bbf9f6
ashiqYousuf <ashiqhussainkumar143@gmail.com>
2024-03-09 17:42:31 +0530

    Added secureHeaders middleware

---
+++ cmd/web/middleware.go
@@ -0,0 +1,17 @@
+package main
+
+import "net/http"
+
+// ? Flow:- secureHeaders → servemux → application handler → servemux → secureHeaders
+func secureHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; fontsrc fonts.gstatic.com")
+		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-Frame-Options", "deny")
+		w.Header().Set("X-XSS-Protection", "0")
+		// ! code before view
+		next.ServeHTTP(w, r)
+		// ! code after view
+	})
+}

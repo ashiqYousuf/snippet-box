7bc1798db56202ec25612f5594af15cc3a8d35f0
ashiqYousuf <ashiqhussainkumar143@gmail.com>
2024-03-09 20:39:26 +0530

    Recovery Middleware added

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 // ? Flow:- secureHeaders → servemux → application handler → servemux → secureHeaders
 func secureHeaders(next http.Handler) http.Handler {
@@ -13,5 +16,24 @@ func secureHeaders(next http.Handler) http.Handler {
 		// ! code before view
 		next.ServeHTTP(w, r)
 		// ! code after view
+	})
+}
+
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+				app.serverError(w, fmt.Errorf("%s", err))
+			}
+		}()
+		next.ServeHTTP(w, r)
 	})
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/ashiqYousuf/snippet-box
 go 1.21.5
 
 require github.com/go-sql-driver/mysql v1.7.1
+
+require github.com/justinas/alice v1.2.0

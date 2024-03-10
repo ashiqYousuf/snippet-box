7cd5e00007a53e41f16a258c408a6ad55c0c5ebe
ashiqYousuf <ashiqhussainkumar143@gmail.com>
2024-03-10 21:24:31 +0530

    Using sessions to show flash msg on snippet create

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -29,6 +29,7 @@ func (app *application) notFound(w http.ResponseWriter) {
 func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
 		CurrentYear: time.Now().Year(),
+		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
 	}
 }
 
